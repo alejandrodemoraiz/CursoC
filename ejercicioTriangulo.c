a1f01f565ca00217ebcc2d19847fcd64aa4340d0
Helalubo <alejandrodemoraiz@gmail.com>
2019-03-13 19:03:00 -0300

    ejercicio 12, no me funciona la libreria math en Visual Studio Code.

---
+++ ejercicioTriangulo.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+ float cateto1, cateto2, hipotenusa;
+ 
+ printf("ingrese los dos catetos");
+scanf("%f %f",&cateto1,&cateto2);
+
+hipotenusa = sqrt(pow(cateto1,2)+ pow(cateto2,2));
+
+
+
+
+printf("la hipotenusa del triangulo es %.2f\n",hipotenusa);
+
+
+
+
+    return 0;
+}
+
+//  sacar la hipotenusa de un triangulo regtangulo, pidiendo al usuario el 
+// valor de los 2 catetos
