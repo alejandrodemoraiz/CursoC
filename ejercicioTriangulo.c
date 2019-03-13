142321cdd0144e4830f64f3c712908d03a5da98a
Helalubo <alejandrodemoraiz@gmail.com>
2019-03-13 19:14:42 -0300

    solucionado ejercicio 12, en vez de usar gcc para crear acceso en la consola de visual studio code usar g++ para crear el ejecutable, inmediiatamente se ejecuta en la terminal.

--- ejercicioTriangulo.c
+++ ejercicioTriangulo.c
@@ -3,7 +3,7 @@
 int main()
 {
  float cateto1, cateto2, hipotenusa;
- 
+
  printf("ingrese los dos catetos");
 scanf("%f %f",&cateto1,&cateto2);
 
@@ -20,5 +20,5 @@ printf("la hipotenusa del triangulo es %.2f\n",hipotenusa);
     return 0;
 }
 
-//  sacar la hipotenusa de un triangulo regtangulo, pidiendo al usuario el 
+//  sacar la hipotenusa de un triangulo regtangulo, pidiendo al usuario el
 // valor de los 2 catetos
