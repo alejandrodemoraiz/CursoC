0a9e0260322acb268fb26d741682fb0dd2d40131
Helalubo <alejandrodemoraiz@gmail.com>
2019-03-13 19:28:41 -0300

    no olvidar el & para igualar.

---
+++ ejercicioCircunferencia.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    int ladoDeCuadrado, totalCircunferencia;
+
+    printf("ingrese el lado del cuadrado");
+    scanf("%i",&ladoDeCuadrado);
+
+    totalCircunferencia = (ladoDeCuadrado*4);
+
+    printf("la circunferencia del cuadrado es: %i\n",totalCircunferencia);
+
+
+
+
+
+    return 0;
+}
