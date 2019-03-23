0674558c22f20a1b80b38e6902a22704180a7742
Helalubo <alejandrodemoraiz@gmail.com>
2019-03-23 02:34:06 -0300

    solucionado, usar antes del printf pidiendo las letras el fflush y luego getchar() y  por ultimo el scanf guardando la letra ...asi funciona la validacion de letras

--- ParcialesDeIngresoEnC/ejercicio6/main.c
+++ ParcialesDeIngresoEnC/ejercicio6/main.c
@@ -2,33 +2,42 @@
 #include <stdlib.h>
 #include <math.h>
 #include <string.h>
+#include <ctype.h>
 int main()
 {
-  int nota, contador =0, acumuladorNotas =0;
+  int nota;
+  int contador =0;
+  int acumuladorNotas =0;
   float promedio;
-  char sexo[15];
+  char sexo;
 
 
     while (contador <5)
     {
         contador ++;
         printf("Ingrese la nota del alumno # %d", contador);
+             fflush(stdin);
         scanf("%d", &nota);
 
         while(nota < 1 || nota >10)
         {
-           printf(" ERROR! Ingrese la nota del alumno # %d ", contador);
+           printf(" ERROR! Ingrese la nota del alumno # %d ",contador);
+                fflush(stdin);
             scanf("%d", nota);
 
         }
 
-        printf("Ingrese sexo f 0 m del alumno # %d", contador);
-        scanf("%s", sexo);
+        printf("Ingrese sexo f 0 m del alumno # %d",contador);
+        fflush(stdin);
+        sexo = getchar();
+        scanf("%c",&sexo);
 
-   while(sexo != "f" && sexo != "m")
+   while(sexo != 'f' && sexo != 'm')
         {
                 printf("ERROR! ingrese sexo valido, m para masculino, f para femenino");
-                scanf("%s", sexo);
+                fflush(stdin);
+                sexo = getchar();
+                scanf("%c", &sexo);
         }
 
 
