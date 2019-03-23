d7d71bf3d9f160330682b84db0b9beb2d29bb261
Helalubo <alejandrodemoraiz@gmail.com>
2019-03-23 00:12:41 -0300

    nno me funciona la validacion de sexo

--- ParcialesDeIngresoEnC/ejercicio6/main.c
+++ ParcialesDeIngresoEnC/ejercicio6/main.c
@@ -6,7 +6,7 @@ int main()
 {
   int nota, contador =0, acumuladorNotas =0;
   float promedio;
-  char sexo [50];
+  char sexo[15];
 
 
     while (contador <5)
@@ -18,17 +18,17 @@ int main()
         while(nota < 1 || nota >10)
         {
            printf(" ERROR! Ingrese la nota del alumno # %d ", contador);
-            scanf("%s", nota);
+            scanf("%d", nota);
 
         }
 
         printf("Ingrese sexo f 0 m del alumno # %d", contador);
         scanf("%s", sexo);
 
-   while (sexo != "f" && sexo != "m") // No me funciona la valiidacion.
+   while(sexo != "f" && sexo != "m")
         {
-          printf("ERROR! Ingrese sexo :m 0 f del alumno # %d", contador);
-                scanf("%s",sexo);
+                printf("ERROR! ingrese sexo valido, m para masculino, f para femenino");
+                scanf("%s", sexo);
         }
 
 
---
+++ ParcialesDeIngresoEnC/jajaja/main.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{
+  int nota, contador =0, acumuladorNotas =0;
+  float promedio;
+  char sexo;
+
+
+    while (contador <5)
+    {
+        contador ++;
+        printf("Ingrese la nota del alumno # %d", contador);
+
+        scanf("%d",&nota);
+
+        while(nota < 1 || nota >10)
+        {
+
+           printf(" ERROR! Ingrese la nota del alumno # %d ", contador);
+            scanf("%d", nota);
+
+        }
+
+        printf("Ingrese sexo f 0 m del alumno # %d", contador);
+        fflush(stdin);
+        scanf("%s",sexo);
+
+   while(sexo != "m" && sexo != "f")
+        {
+
+                printf("ERROR! ingrese sexo valido, m para masculino, f para femenino");
+                  fflush(stdin);
+                scanf("%s",sexo);
+        }
+
+    }
+            return 0;
+        }
