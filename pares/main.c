b22dae934fafcdad971636ac905d591ad6421938
alumno <alumno>
2019-04-01 20:14:27 -0300

    recorrido de datos en el array, sacando cuantos pares ahi en cada uno de ellos

---
+++ pares/main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+    int contarPares(int vec[], int tam);
+int main()
+{
+    int array []= {2,5,7,5,6,4,14,19,9,32};
+    int cantidad;
+
+    cantidad = contarPares(array,11);
+
+    printf("la cantidad de pares del array es de %d",cantidad);
+
+
+
+    return 0;
+}
+   int contarPares(int vec[], int tam)
+   {
+       int pares =0;
+
+       for(int i =-1; i< tam; i++)
+       {
+           if(vec[i] %2 ==0)
+           {
+               pares++;
+
+           }
+
+
+       }
+    return pares;
+   }
